e2159d0a5410ba944a43462fe900f6daa710336c
clark <clark@championtek.com.tw>
2020-05-12 15:00:24 +0800

    修正 config

--- README.md
+++ README.md
@@ -63,8 +63,3 @@ func (h *ApiHandle) GetHello() {
 ```
 
 iris MVC 相關操作請見[官方文件](https://github.com/kataras/iris/wiki/MVC)
-
-
-
-* Local端log放置目錄為 logs
-* 當設置檔內 `LoggerEnable` 為 `true` 時，紀錄會上傳到 `ELK伺服器`
--- tests/service_test.go
+++ tests/service_test.go
@@ -5,13 +5,6 @@ import (
 
 	"github.com/kataras/iris/v12"
 
-	"git.championtek.com.tw/go/champiris/middleware/jwtpassport"
-	"git.championtek.com.tw/go/passport"
-	"github.com/kataras/iris/v12/context"
-
-	"git.championtek.com.tw/go/champiris/middleware/elklogger"
-	"git.championtek.com.tw/go/logger/v2"
-
 	"github.com/kataras/iris/v12/mvc"
 
 	"git.championtek.com.tw/go/champiris"
@@ -30,9 +23,7 @@ func TestApi_HelloWord(t *testing.T) {
 	var service champiris.Service
 	var config champiris.NetConfig
 	config = champiris.NetConfig{
-		Port:         "8080",
-		LoggerEnable: false,
-		JWTEnable:    false}
+		Port: "8080"}
 	service.New(&config)
 	service.AddRoute(champiris.RouterSet{
 		Party:  "/api",
@@ -56,55 +47,15 @@ func (h *ApiHandle) GetHello() {
 
 func TestAPI_NewService(t *testing.T) {
 	var service champiris.Service
-	var elk context.Handler
-	var psp context.Handler
 
 	_ = service.New(&champiris.NetConfig{
-		Port:         "8080",
-		LoggerEnable: true,
-		JWTEnable:    false,
-	})
-
-	if service.Config.LoggerEnable {
-		// if LoggerEnable is true, then setup logger
-		elk = elklogger.New(&logger.ELKConfig{ELK: logger.ELKInfo{
-			URL:              "http://52.196.196.142",
-			Port:             "9200",
-			Type:             logger.Graphql,
-			Index:            "champ_iris",
-			User:             "elastic",
-			Password:         "work$t/6qup3",
-			NumberOfShards:   1,
-			NumberOfReplicas: 0,
-		}})
-	}
-
-	if service.Config.JWTEnable {
-		// if JWTEnable is true, then setup jwt
-		psp = jwtpassport.New(&passport.Config{
-			Secret: "champ",
-			Claims: passport.PublicClaims{
-				Audience:    "bskini",
-				Subject:     "champiris",
-				Email:       "service@dishrank.com",
-				PhoneNumber: "",
-				Issuer:      "drc",
-				Duration:    3600,
-			},
-		})
-	}
+		Port: "8080"})
 
 	service.App.Logger().SetLevel("debug")
 	router := champiris.RouterSet{
 		Party: "/service/v1",
 		Router: func(m *mvc.Application) {
 			m.Router.Use(cors.AllowAll())
-			if elk != nil {
-				m.Router.Use(elk)
-			}
-			if psp != nil {
-				m.Router.Use(psp)
-			}
 			m.Handle(Ql)
 		},
 	}
