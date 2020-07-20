fce4d59d4f2664293790d402072bfaf38fcf9d2e
clark <clark@championtek.com.tw>
2020-07-20 15:32:20 +0800

    1.刪除原靜態網頁位置 2.預設伺服器 不需傳入設定值 3.刪除Graphql 相關測試

--- tests/service_test.go
+++ tests/service_test.go
@@ -8,24 +8,12 @@ import (
 	"github.com/kataras/iris/v12/mvc"
 
 	"git.championtek.com.tw/go/champiris"
-	"git.championtek.com.tw/go/champiris-contrib/graphql"
-	"github.com/iris-contrib/middleware/cors"
 )
-
-var Ql *graphql.Graphql
-
-func init() {
-	Ql = graphql.Default()
-	Ql.ShowPlayground = true
-}
 
 func TestApi_HelloWord(t *testing.T) {
 	var service champiris.Service
-	var config champiris.NetConfig
-	config = champiris.NetConfig{
-		Host: "0.0.0.0",
-		Port: "8080"}
-	service.New(&config)
+
+	service.Default()
 	service.AddRoute(champiris.RouterSet{
 		Party:  "/api",
 		Router: apiRouter,
@@ -44,25 +32,4 @@ type ApiHandle struct {
 //get localhost:port/api/hello
 func (h *ApiHandle) GetHello() {
 	h.Ctx.WriteString("HelloWord")
-}
-
-func TestAPI_NewService(t *testing.T) {
-	var service champiris.Service
-
-	_ = service.New(&champiris.NetConfig{
-		Port: "8080"})
-
-	service.App.Logger().SetLevel("debug")
-	router := champiris.RouterSet{
-		Party: "/service/v1",
-		Router: func(m *mvc.Application) {
-			m.Router.Use(cors.AllowAll())
-			m.Handle(Ql)
-		},
-	}
-	service.AddRoute(router)
-	addSchema()
-	if err := service.Run(); err != nil {
-		t.Error(err)
-	}
 }
